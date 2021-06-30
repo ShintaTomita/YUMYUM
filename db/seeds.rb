924ab568681006a5c16981091bae40f4975ae6f9
ShintaTomita <shinta.dopeman@gmail.com>
2021-06-30 15:30:45 +0000

    edit seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Genre.create!(id: 1, genre_name: "主食", price: "500")
+Genre.create!(id: 2, genre_name: "副食", price: "300")
+Genre.create!(id: 3, genre_name: "スープ", price: "200")
+Genre.create!(id: 4, genre_name: "ドレッシング・ソース", price: "150")
+Genre.create!(id: 5, genre_name: "デザート", price: "150")
