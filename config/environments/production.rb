a21dc15fe72aa726ebce0cdb178dcd97001cd8d7
ShintaTomita <shinta.dopeman@gmail.com>
2021-07-01 13:47:38 +0000

    edit for s3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -104,7 +104,7 @@ Rails.application.configure do
     s3_credentials: {
       access_key_id: ENV['AWS_ACCESS_KEY_ID'],
       secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-      s3_region: 'ap-northeast-1'
+      s3_region: 'ap-northeast-3'
     },
     bucket: ENV['S3_BUCKET']
   }
