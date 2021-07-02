bcbdffb8118bd4aed8fbfc7b3e15791a1be2ba29
ShintaTomita <shinta.dopeman@gmail.com>
2021-07-02 03:32:24 +0000

    edit webpack

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -3,3 +3,11 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 const environment = require('./environment')
 
 module.exports = environment.toWebpackConfig()
+
+module.exports = {
+
+  performance: {
+    maxEntrypointSize: [350,000],
+    maxAssetSize: [350,000]
+  }
+}
